322f4a2b549a95a6e1de50103f782d33fd70facc
Artsiom Rak <ra44elo@gmail.com>
2019-03-24 20:39:53 +0300

    feat: add part of implementation of node.js file

--- src/node.js
+++ src/node.js
@@ -1,22 +1,45 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		node.parent = this;	
+		if(this.left == null) {
+			this.left = node;
+		} else if(this.left != null && this.right == null) {
+			this.right = node;
+		}
 	}
 
 	removeChild(node) {
-
+		this.left.parent = null;
+		if(this.left == node) {
+			this.left = null;
+		}
+		else if(this.right == node) {
+			this.right = null;
+		}
+		else if(node != this.left || node != this.right) {
+			throw new Error;
+		}
 	}
 
 	remove() {
-
+		if (this.parent != null) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (this.parent != null) {
+			const swap = this.parent;
+			this.parent = this;
+		}
 	}
 }
 
